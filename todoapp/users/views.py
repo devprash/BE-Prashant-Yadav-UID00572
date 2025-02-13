f91656b31cd4b3c6603726f1d6d3815ef05aeb7f
prashant <prashant.yadav@joshtechnologygroup.com>
2025-02-13 15:49:47 +0530

    Exercise9and10 - Added user registration and login

--- todoapp/users/views.py
+++ todoapp/users/views.py
@@ -1,4 +1,15 @@
-class UserRegistrationAPIView():
+from django.contrib.auth import authenticate
+
+from rest_framework import status
+from rest_framework.authtoken.models import Token
+from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from users import serializers as user_serializers
+
+
+class UserRegistrationAPIView(APIView):
     """
         success response format
          {
@@ -9,12 +20,45 @@ class UserRegistrationAPIView():
            "token"
          }
     """
+    permission_classes = [AllowAny]
+
+    def post(self, request, *args, **kwargs):
+        serializer = user_serializers.UserRegistrationSerializer(
+            data=request.data)
+
+        if not serializer.is_valid():
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+        user = serializer.save()
+        token, _ = Token.objects.get_or_create(user=user)
+
+        response_data = serializer.data
+        response_data['token'] = token.key
+
+        return Response(response_data, status=status.HTTP_201_CREATED)
 
 
-class UserLoginAPIView():
+class UserLoginAPIView(APIView):
     """
         success response format
          {
            auth_token: ""
          }
     """
+    permission_classes = [AllowAny]
+
+    def post(self, request, *args, **kwargs):
+        serializer = user_serializers.UserLoginSerializer(data=request.data)
+
+        if not serializer.is_valid():
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+        email = serializer.validated_data['email']
+        password = serializer.validated_data['password']
+        user = authenticate(username=email, password=password)
+
+        if user is None:
+            return Response({'error': 'Invalid Credentials'}, status=status.HTTP_401_UNAUTHORIZED)
+
+        token, created = Token.objects.get_or_create(user=user)
+        return Response({'auth-token': token.key}, status=status.HTTP_200_OK)
