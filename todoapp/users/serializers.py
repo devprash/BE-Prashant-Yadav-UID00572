905725cd7c9893fc5fd4e654178ee79e5ac539eb
prashant <prashant.yadav@joshtechnologygroup.com>
2025-02-13 15:29:06 +0530

    Revert "exercise9and10"
    This reverts commit efdbcbe6310cfb7e509c47700e073e5e5ab215ca.

--- todoapp/users/serializers.py
+++ todoapp/users/serializers.py
@@ -41,35 +41,3 @@ class UserProjectSerializer(serializers.ModelSerializer):
             "to_do_projects", "in_progress_projects",
             "completed_projects"
         ]
-
-
-class UserRegistrationSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(write_only=True)
-    email = serializers.EmailField(required=True)
-
-    class Meta:
-        model = user_models.CustomUser
-        fields = ['email', 'password', 'first_name',
-                  'last_name', 'date_joined']
-        read_only_fields = ['date_joined']
-
-    def validate_email(self, value):
-        if user_models.CustomUser.objects.filter(email=value).exists():
-            raise serializers.ValidationError(
-                "A user with this email already exists."
-            )
-        return value
-
-    def create(self, validated_data):
-        user = user_models.CustomUser.objects.create_user(
-            password=validated_data['password'],
-            email=validated_data['email'],
-            first_name=validated_data.get('first_name', ''),
-            last_name=validated_data.get('last_name', '')
-        )
-        return user
-
-
-class UserLoginSerializer(serializers.Serializer):
-    email = serializers.EmailField(required=True)
-    password = serializers.CharField(write_only=True, required=True)
